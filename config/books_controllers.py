7181d37a1b35f31a54c59be92d44058cd2f8c6d0
MateoNaranjo <mateo.naranjo.artunduaga@gmail.com>
2025-09-07 18:38:13 -0500

    Initial commit

---
+++ config/books_controllers.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, request
+
+books_bp = Blueprint('books_bp', __name__)
+
+@books_bp.route('/books', methods=['GET'])
+
+@books_bp.route('/books/<int:book_id>', methods=['GET'])
+
+@books_bp.route('/books', methods=['POST'])
+
+@books_bp.route('/books/<int:book_id>', methods=['PUT'])
+
+@books_bp.route('/books/<int:book_id>', methods=['DELETE'])
+
